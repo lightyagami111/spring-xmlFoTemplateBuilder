a9dadacdba62294980da31a8d796c06c156b0c65
Ivaylo Rusev <ivaylo.rusev.work@gmail.com>
2020-08-27 06:22:09 +0300

    maybe final version

--- src/test/java/com/ivaylorusev/xmlFoTemplateBuilder/BasicApplicationTests.java
+++ src/test/java/com/ivaylorusev/xmlFoTemplateBuilder/BasicApplicationTests.java
@@ -17,49 +17,74 @@ public class BasicApplicationTests {
 
     @Test
     public void parse() throws Exception {
-        MasterRequest masterRequestDump = new MasterRequest(Brand.VW, MailTypeVariant.ACTIVATION,
+        MasterRequest masterRequestDump = new MasterRequest(Brand.VW, MailTypeVariant.VOUCHER_DELIVERY,
                                                             new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
                                                             new PaymentData(PaymentType.CC_MASTER),
                                                 "444444");
 
         Dump.dumpToXlsFile("format", attachmentsMustacheService.buildAttachmentTemplate(masterRequestDump));
 
-        /*
-        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.CREDIT_NOTE,
-                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
-                new PaymentData(PaymentType.CC_MASTER),
-                null));
-
-        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.CREDIT_NOTE,
-                new CustomerInformation(Salutation.MR, CustomerType.PRIVATE),
-                new PaymentData(PaymentType.CC_MASTER),
-                null));
-
-        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.CREDIT_NOTE,
-                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
-                new PaymentData(PaymentType.PREPAID),
-                null));
-
-        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.CREDIT_NOTE,
-                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
-                new PaymentData(PaymentType.CC_MASTER),
-                "54654654645"));
-
-        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.ACTIVATION,
-                new CustomerInformation(Salutation.MR, CustomerType.PRIVATE),
-                new PaymentData(PaymentType.CC_VISA),
-                null));
-
-        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.VOUCHER_INVOICE_DE,
-                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
-                new PaymentData(PaymentType.CC_MASTER),
-                null));
-
-        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.VOUCHER_INVOICE_EU,
-                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
-                new PaymentData(PaymentType.CC_MASTER),
-                null));
-         */
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.CREDIT_NOTE,
+//                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
+//                new PaymentData(PaymentType.CC_MASTER),
+//                null));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.CREDIT_NOTE,
+//                new CustomerInformation(Salutation.MR, CustomerType.PRIVATE),
+//                new PaymentData(PaymentType.CC_MASTER),
+//                null));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.CREDIT_NOTE,
+//                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
+//                new PaymentData(PaymentType.PREPAID),
+//                null));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.CREDIT_NOTE,
+//                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
+//                new PaymentData(PaymentType.CC_MASTER),
+//                "54654654645"));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.ACTIVATION,
+//                new CustomerInformation(Salutation.MR, CustomerType.PRIVATE),
+//                new PaymentData(PaymentType.CC_VISA),
+//                null));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.VOUCHER_INVOICE_DE,
+//                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
+//                new PaymentData(PaymentType.CC_MASTER),
+//                null));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.VW, MailTypeVariant.VOUCHER_INVOICE_EU,
+//                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
+//                new PaymentData(PaymentType.CC_MASTER),
+//                null));
+//
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.SKODA, MailTypeVariant.CREDIT_NOTE,
+//                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
+//                new PaymentData(PaymentType.CC_MASTER),
+//                null));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.SKODA, MailTypeVariant.CREDIT_NOTE,
+//                new CustomerInformation(Salutation.MR, CustomerType.PRIVATE),
+//                new PaymentData(PaymentType.CC_MASTER),
+//                null));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.SKODA, MailTypeVariant.CREDIT_NOTE,
+//                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
+//                new PaymentData(PaymentType.PREPAID),
+//                null));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.SKODA, MailTypeVariant.CREDIT_NOTE,
+//                new CustomerInformation(Salutation.MR, CustomerType.COMPANY),
+//                new PaymentData(PaymentType.CC_MASTER),
+//                "54654654645"));
+//
+//        attachmentsMustacheService.buildAttachmentTemplate(new MasterRequest(Brand.SKODA, MailTypeVariant.ACTIVATION,
+//                new CustomerInformation(Salutation.MR, CustomerType.PRIVATE),
+//                new PaymentData(PaymentType.CC_VISA),
+//                null));
 
     }
 
